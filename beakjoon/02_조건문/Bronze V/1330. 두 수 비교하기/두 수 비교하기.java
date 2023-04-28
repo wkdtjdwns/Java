623b24ffefbcd673a29a3a386d1881487738bbab
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-04-28 16:53:19 +0900

    Update and rename beakjoon/02_조건문/_01_두수비교하기 to beakjoon/02_조건문/Bronze V/1330. 두 수 비교하기/두 수 비교하기.java

---
+++ beakjoon/02_조건문/Bronze V/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+class _01_두수비교하기 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int A, B;
+    A = sc.nextInt();
+    B = sc.nextInt();
+    sc.close();
+		
+    // 삼항연산자는 중첩할 수 있다.
+    // A가 B보다 크면 >, 작으면 <, 같으면 ==를 str변수에 대입함    
+    String str = (A > B) ? ">" : (A < B) ? "<" : "=="; 
+    System.out.println(str);
+    }
+}
