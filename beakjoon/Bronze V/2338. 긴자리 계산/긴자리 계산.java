c037b5d0b26b044040668581e30983eb29af7f28
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-04-28 17:33:27 +0900

    Update 긴자리 계산.java

--- beakjoon/Bronze V/2338. 긴자리 계산/긴자리 계산.java
+++ beakjoon/Bronze V/2338. 긴자리 계산/긴자리 계산.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 import java.math.BigInteger;
 
 class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		BigInteger a = sc.nextBigInteger();
-		BigInteger b = sc.nextBigInteger();
-		System.out.println(a.add(b)); // 더하기
-		System.out.println(a.subtract(b)); // 빼기
-		System.out.println(a.multiply(b)); // 곱하기
-	}
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    BigInteger a = sc.nextBigInteger();
+    BigInteger b = sc.nextBigInteger();
+    System.out.println(a.add(b)); // 더하기
+    System.out.println(a.subtract(b)); // 빼기
+    System.out.println(a.multiply(b)); // 곱하기
+    }
 }
