8ea9295b0363d99ff3f867485a3f909e4a076fc7
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-04-30 19:15:22 +0900

    Update 사파리월드.java

--- beakjoon/Bronze V/2420. 사파리월드/사파리월드.java
+++ beakjoon/Bronze V/2420. 사파리월드/사파리월드.java
@@ -7,10 +7,10 @@ import java.lang.Math;
 public class Main {
     
     public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    long n = Long.parseLong(st.nextToken());
-    long m = Long.parseLong(st.nextToken());
-    System.out.println(Math.abs(n-m)); // 빼고 나서 절댓값 출력 -> 양수
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+        System.out.println(Math.abs(n-m)); // 빼고 나서 절댓값 출력 -> 양수
     }
 }
