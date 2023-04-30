0a184c7639fbc5ccdc4b42e771f30e44c697c6a8
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-04-30 19:16:45 +0900

    Update A+B - 2.java

--- beakjoon/Bronze V/2558. A+B - 2/A+B - 2.java
+++ beakjoon/Bronze V/2558. A+B - 2/A+B - 2.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
-class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		System.out.println(a+b);
-	}
+public class Main {
+	
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	sc.close()
+	System.out.println(a+b);
+    }
 }
