faca8780c020dd7ed615059bc1aec8d28b29fcd0
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-04-30 19:50:57 +0900

    Update 대소문자 바꾸기.java

--- beakjoon/Bronze V/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ beakjoon/Bronze V/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -4,21 +4,21 @@ import java.io.IOException;
 
 public class Main {
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-		for (int i=0; i<str.length(); i++) 
-		{
-			char c = str.charAt(i);
-			if (Character.isUpperCase(c)) // 문자열 str의 i번째 문자가 대문자면
-			{
-				// 줄바꿈 없이 그 문자의 소문자 출력
-				System.out.print(String.valueOf(c).toLowerCase());
-			} else // 문자열 str의 i번째 문자가 대문자가 아니면 -> 소문자면
-			{
-				// 줄바꿈 없이 그 문자의 대문자 출력
-				System.out.print(String.valueOf(c).toUpperCase());
-			}
-		}
+    public static void main(String[] args) throws IOException {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	String str = br.readLine();
+	for (int i=0; i<str.length(); i++) 
+	{
+	    char c = str.charAt(i);
+	    if (Character.isUpperCase(c)) // 문자열 str의 i번째 문자가 대문자면
+	    {
+	        // 줄바꿈 없이 그 문자의 소문자 출력
+		System.out.print(String.valueOf(c).toLowerCase());
+	    } else // 문자열 str의 i번째 문자가 대문자가 아니면 -> 소문자면
+	    {
+	        // 줄바꿈 없이 그 문자의 대문자 출력
+	        System.out.print(String.valueOf(c).toUpperCase());
+	    }
 	}
+    }
 }
