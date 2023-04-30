0b4be81ae14ed1cd19db9c9e3dfc7afedcb12772
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-04-30 19:09:30 +0900

    Update A÷B.java

--- beakjoon/Bronze V/1008. A÷B/A÷B.java
+++ beakjoon/Bronze V/1008. A÷B/A÷B.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
 	Scanner in = new Scanner(System.in);
 	// 오차 범위를 벗어나지 않게 하기 위해서는 반드시 double형으로 출력을 해주어야 한다.
