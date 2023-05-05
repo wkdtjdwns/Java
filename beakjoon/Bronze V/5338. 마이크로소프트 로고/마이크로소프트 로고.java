26de48c10cb5b29ee3756632ad923fdf2fc75488
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-05-05 12:54:18 +0900

    Update 마이크로소프트 로고.java

--- beakjoon/Bronze V/5338. 마이크로소프트 로고/마이크로소프트 로고.java
+++ beakjoon/Bronze V/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -2,9 +2,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("       _.-;;-._");
-        System.out.println("'-..-'|   ||   |");
-        System.out.println("'-..-'|_.-;;-._|");
-        System.out.println("'-..-'|   ||   |");
-        System.out.print("'-..-'|_.-''-._|");
+        	System.out.println("'-..-'|   ||   |");
+       		System.out.println("'-..-'|_.-;;-._|");
+        	System.out.println("'-..-'|   ||   |");
+        	System.out.print("'-..-'|_.-''-._|");
 	}
 }
