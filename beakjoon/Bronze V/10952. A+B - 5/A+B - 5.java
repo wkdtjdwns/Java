bf510e4166bc65e6c0a8f5742f7fe01f636356e9
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-05-07 12:35:31 +0900

    Create A+B - 5.java

---
+++ beakjoon/Bronze V/10952. A+B - 5/A+B - 5.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();	
+		StringTokenizer st;
+		while (true) 
+		{
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if ( (a + b) == 0 ) 
+			{
+				break;
+			}
+			sb.append(a+b).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
