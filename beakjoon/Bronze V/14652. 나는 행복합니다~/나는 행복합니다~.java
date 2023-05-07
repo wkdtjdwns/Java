8378bd39ac7d4f0fa2a5708692d0b198c5f23ca7
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-05-07 15:11:18 +0900

    Update 나는 행복합니다~.java

--- beakjoon/Bronze V/14652. 나는 행복합니다~/나는 행복합니다~.java
+++ beakjoon/Bronze V/14652. 나는 행복합니다~/나는 행복합니다~.java
@@ -6,8 +6,8 @@ import java.util.StringTokenizer;
 public class Main {
   
 	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
-		String str = bf.readLine();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		String str = br.readLine();
 		StringTokenizer st = new StringTokenizer(str); 
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
