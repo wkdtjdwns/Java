fa6933f8f0daf21d8d01d8dddd27ea48f928efa4
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-05-07 15:10:23 +0900

    Create 나는 행복합니다~.java

---
+++ beakjoon/Bronze V/14652. 나는 행복합니다~/나는 행복합니다~.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
+		String str = bf.readLine();
+		StringTokenizer st = new StringTokenizer(str); 
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		System.out.println(k/m + " " + k%m);
+  }
+}
