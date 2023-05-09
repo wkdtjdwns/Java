b817551150abb5677872a73e10fbcfcda1d1f02e
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-05-09 18:48:57 +0900

    Create 큰 수 (BIG).java

---
+++ beakjoon/Bronze V/14928. 큰 수 (BIG)/큰 수 (BIG).java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+  
+	    public static void main(String[] args) throws IOException {
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	        //StringTokenizer st = new StringTokenizer(br.readLine());
+
+	        //시간초과가 나온 코드
+	        /*BigInteger N = new BigInteger(st.nextToken());
+	        BigInteger M = new BigInteger("20000303");
+	        System.out.println(N.remainder(M));*/
+
+	        //나머지 연산 방법
+	        String N = br.readLine();
+	        long remain = 0;
+	        for(int i =0; i < N.length(); i++){
+	            remain = (remain * 10 + (N.charAt(i) - '0')) % 20000303;
+	        }
+	        bw.write(remain + "\n");
+	        bw.flush();
+	        br.close();
+	        bw.close();	
+	}
+}
