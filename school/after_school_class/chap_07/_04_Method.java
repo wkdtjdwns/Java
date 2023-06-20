b1610ddca15200ae43b68bcdfc223e9241c2fc4f
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-06-20 17:43:17 +0900

    Update _04_Method.java

--- school/after_school_class/chap_07/_04_Method.java
+++ school/after_school_class/chap_07/_04_Method.java
@@ -14,5 +14,18 @@ public class _04_Method {
 		b1.autoReport();
 		
 		b1.insertMemoryCard(512);
+		
+		// 전체 영상 : type = 0
+		// 일반 영상 : type = 1
+		// 이벤트 영상(충돌 감지) : type = 2
+			
+		int fileCount = b1.getVideoFileCount(0); // 전체 영상 갯수
+		System.out.println("전체 영상 파일 수 : " + fileCount);
+		
+		fileCount = b1.getVideoFileCount(1); // 일반 영상 갯수
+		System.out.println("일반 영상 파일 수 : " + fileCount);
+		
+		fileCount = b1.getVideoFileCount(2); // 이벤트 영상 갯수
+		System.out.println("이벤트 영상 파일 수 : " + fileCount);
 	}
 }
