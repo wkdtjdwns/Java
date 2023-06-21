efdfcb584514fc77dfda1d216562e3a5fbae4b6f
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-06-21 17:20:11 +0900

    Update BlackBox.java

--- school/after_school_class/chap_07/BlackBox.java
+++ school/after_school_class/chap_07/BlackBox.java
@@ -94,4 +94,105 @@ public class BlackBox {
 		// 위에서 선언한 메소드를 불러옴
 		record(true, true, 5);
 	}
+	
+	static void callServieceCenter()
+	{
+		System.out.println("서비스센터(1588-0000)으로 연결합니다.");
+		
+//		modelName = "초록이";
+//		같은 파일에서 접근 했지만 오류가 생김
+//		-> 인스턴스 필드는 객체화 되어야지만 활용할 수 있기 때문
+		
+//		canAutoReport = false;
+//		반면에 클래스 필드는 상관 없음
+		
+	}
+
+//	복습
+//	
+//	// 필드
+//	// 인스턴스 필드 - 각 개체 안에서 사용되는 필드
+//	String modelName; // 모델명
+//	String resolution; // 해상도
+//	int price; // 가격
+//	String color; // 색상
+//	
+//	// 클래스 필드 - 모든 객체에 공통적으로 적용하는 필드 (메모리에 올려놓음 -> static)
+//	static boolean canAutoReport = false;
+//	
+//	// 생성자 : 클래스 이름과 같은 메소드
+//	// 아래와 같이 빈 코드를 갖는 생성자를 기본생성자라고 함
+//	// 개발자가 생성자를 따로 쓰지 않은 경우에는 자바에서 자동으로 기본 생성자를 생성해 줌
+//	public void BlackBox() {}
+//	
+//	// 메소드 : 기능을 담당함
+//	// 인스턴스 메소드 - 해당 객체에서만 사용할 수 있는 메소드
+//	
+//	void autoReport() // 파라미터 X , 리턴값 X
+//	{
+//		if (canAutoReport)
+//		{
+//			System.out.println("충돌이 감지되어 자동으로 신고합니다.");
+//		}
+//		
+//		else
+//		{
+//			System.out.println("자동 신고 기능이 지원되지 않습니다.");
+//		}
+//	}
+//	
+//	void insertMemoryCard(int capacity) // 파라미터 O, 리턴값 X
+//	{
+//		System.out.println("메모리 카드가 삽입되었습니다.");
+//		System.out.println("용량은 " + capacity + "GB 입니다.");
+//	}
+//	
+//	int getVideoFileCount(int type) // 파라미터 O, 리턴값 O
+//	{
+//		if (type == 1) // 일반 영상
+//		{
+//			return 9;
+//		}
+//		
+//		else if (type == 2) // 이벤트 영상
+//		{
+//			return 2;
+//		}
+//		
+//		else // 전체 영상
+//		{
+//			return 11;
+//		}
+//	}
+//		
+//	// showDateTime : 날짜/시간 정보
+//	// showSpeed : 속도
+//	// min : 영상 기록 단위
+//	void record(boolean showDateTime, boolean showSpeed, int min)
+//	{
+//		System.out.println("녹화를 시작합니다.");
+//				
+//		if (showDateTime)
+//		{
+//			System.out.println("영상에 날짜/시간 정보가 표시됩니다.");
+//		}
+//				
+//		else if (showSpeed)
+//		{
+//			System.out.println("영상에 속도 정보가 표시됩니다.");
+//		}
+//				
+//		System.out.println("영상은 " + min + "분 단위로 기록됩니다.");
+//	}
+//	
+//	// 오버로딩 : 파라미터의 갯수가 다름
+//	void record(int min) // 파라미터 1개
+//	{
+//		record(true, true, min);
+//	}
+//	
+//	void record() // 파라미터 0개
+//	{
+//		record(true, true, 5);
+//	}
 }
