0a7cd9252048c2b6bfd446e9e8ed5792ecb17793
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-07-11 17:43:11 +0900

    Update Ex06_1.java

--- school/after_school_class/test/Ex06_1.java
+++ school/after_school_class/test/Ex06_1.java
@@ -3,14 +3,16 @@ package test;
 public class Ex06_1 {
 
 	// 메소드를 사용해서 팩토리얼 계산기 만들기
-	
+
+	// 메인 메소드
 	public static void main(String[] args) {
 		 
 		int result = factorial(5);
 		
 		System.out.println(result);
 	}
-	
+
+	// 팩토리얼 계산기 메소드
 	public static int factorial(int num)
 	{
 		
@@ -20,13 +22,13 @@ public class Ex06_1 {
 		// 자기 자신을 불러옴 -> 재귀 함수
 		return num * factorial(num-1);
 		
-		// factorial(5)
-		// -> 5 * factorial(4)
-		// -> 5 * 4 * factorial(3)
-		// -> 5 * 4 * 3 * factorial(2)
-		// -> 5 * 4 * 3 * 2 * factorial(1)
-		
-		// factorial(1) = 1
-		// -> 5 * 4 * 3 * 2 * 1 -> 120
+//		factorial(5)
+//		-> 5 * factorial(4)
+//		-> 5 * 4 * factorial(3)
+//		-> 5 * 4 * 3 * factorial(2)
+//		-> 5 * 4 * 3 * 2 * factorial(1)
+
+//		factorial(1) = 1
+//		-> 5 * 4 * 3 * 2 * 1 -> 120
 	}
 }
