5d63947ea7244e8d2ffc59ad95293b00d2690f95
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-07 17:49:09 +0900

    Create HydrogenCar.java

---
+++ school/after_school_class/Level3/Inheritance/after 2/HydrogenCar.java
@@ -0,0 +1,9 @@
+package extends1.ex3;
+
+public class HydrogenCar extends Car {
+    
+    // 이렇게 클래스를 추가할 때도 상속으로 인해서 훨씬 편해짐.
+    public void fillHydrogen() {
+        System.out.println("수소를 충전합니다.");
+    }
+}
