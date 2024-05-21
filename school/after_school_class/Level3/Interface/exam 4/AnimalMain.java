33ba0747bccaa7adde3ecec8920c3f4d3723b718
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-21 17:54:56 +0900

    Create AnimalMain.java

---
+++ school/after_school_class/Level3/Interface/exam 4/AnimalMain.java
@@ -0,0 +1,26 @@
+package poly.ex6;
+
+public class AnimalMain {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        Bird bird = new Bird();
+        Chicken chicken = new Chicken();
+
+        soundAnimal(dog);
+        soundAnimal(bird);
+        soundAnimal(chicken);
+
+        flyAnimal(bird);
+        flyAnimal(chicken);
+    }
+
+    private static void soundAnimal(AbstractAnimal animal) {
+        animal.sound();
+    }
+
+    private static void flyAnimal(Fly fly) {
+        fly.fly();
+    }
+}
