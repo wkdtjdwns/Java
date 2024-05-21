ff235aeff98a1c39926ea93c4dc99e526f70f43d
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-21 17:49:07 +0900

    Create InterfaceAnimal.java

---
+++ school/after_school_class/Level3/Interface/exam 1/InterfaceAnimal.java
@@ -0,0 +1,9 @@
+package poly.interface1;
+
+// 인터페이스
+// 인터페이스는 class 대신 interface 키워드를 사용하면 됨.
+public interface InterfaceAnimal {
+
+    public abstract void sound();
+    public abstract void move();
+}
