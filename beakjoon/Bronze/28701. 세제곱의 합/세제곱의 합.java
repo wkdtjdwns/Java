d916b38a363d007effc13fc58ad274a1ff544b1e
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-28 19:48:21 +0900

    Create 세제곱의 합.java

---
+++ beakjoon/Bronze/28701. 세제곱의 합/세제곱의 합.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int result = (int)(n * (n + 1) / 2);
+
+        System.out.println(result);
+        System.out.println(result * result);
+        System.out.println(result * result);
+    }
+}
