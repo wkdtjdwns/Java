c56fddecf4cd0226999a4bfa1d9facdb834075e4
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-29 16:33:34 +0900

    Update PayMain2.java

--- school/after_school_class/Level3/Interface/exam 6/exam 6-1/PayMain2.java
+++ school/after_school_class/Level3/Interface/exam 6/exam 6-1/PayMain2.java
@@ -16,11 +16,11 @@ public class PayMain2 {
 			
 			if (payOption.equals("exit")) {
 				System.out.println("프로그램을 종료합니다");
-				break;
+				return;
 			}
 			
 			System.out.print("결제 금액을 입력하세요:");
-			int amount = Integer.parseInt(br.readLine());
+			int amount = Integer.parseInt(br.	readLine());
 			
 			payService.processPay(payOption, amount);
 		}
