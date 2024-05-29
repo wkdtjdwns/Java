b0930da37258a7c3a0ec9be77e5580bd9bdd680f
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-29 16:20:04 +0900

    Create NaverPay.java

---
+++ school/after_school_class/Level3/Interface/exam 6/my solution/NaverPay.java
@@ -0,0 +1,18 @@
+package poly.ex.pay1;
+
+public class NaverPay implements Pay {
+
+	private String name = "naver"; 
+	
+	@Override
+	public boolean pay(int amount) {
+		System.out.println("네이버페이 시스템과 연결합니다.");
+		System.out.println(amount + "원 결제를 시도합니다.");
+		return true;
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+}
