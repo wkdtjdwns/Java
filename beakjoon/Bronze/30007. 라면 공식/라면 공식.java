241e278c04bac1d79f99ee487708f97ae72e5ebd
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-29 12:03:28 +0900

    Create 라면 공식.java

---
+++ beakjoon/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+
+            System.out.println(a * (x - 1) + b);
+        }
+    }
+}
