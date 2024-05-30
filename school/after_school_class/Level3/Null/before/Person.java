4face205ab062a89eac3c64031e4099e5e1977e7
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-30 18:05:59 +0900

    Create Person.java

---
+++ school/after_school_class/Level3/Null/before/Person.java
@@ -0,0 +1,51 @@
+package nullLabel.before;
+
+public class Person {
+
+	private Label name;
+	private Label mail;
+	
+	public Person(Label name, Label mail) {
+		this.name = name;
+		this.mail = mail;
+	}
+	
+	public Person(Label name) {
+		this(name, null);
+	}
+	
+	public void display() {
+		
+		if (name != null) {
+			name.display();
+		}
+		
+		if (mail != null) {
+			mail.display();
+		}
+	}
+	
+	// 모든 클래스의 부모인 Object 클래스의 toString()을 오버라이딩 함.
+	@Override
+	public String toString() {
+		String result = "[ Person:";
+		result += " name = ";
+		
+		if (name == null) {
+			result += "\"(none)\"";
+		} else {
+			result += name;
+		}
+		
+		result += " mail = ";
+		if (mail == null) {
+			result += "\"(none)\"";
+		} else {
+			result += mail;
+		}
+		
+		result += " ]";
+		
+		return result;
+	}
+}
