173886785e52cfa0c03d3a294ae7fef6ab7af3d5
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-02 09:16:00 +0900

    Create 줄번호.java

---
+++ beakjoon/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            System.out.printf("%d. %s\n", (i + 1), str);
+        }
+    }
+}
