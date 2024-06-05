a644e680be2fd22304f0e1b4f2563745772fa8a3
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-05 20:29:20 +0900

    Create 모음의 개수.java

---
+++ beakjoon/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int cnt = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if ( (str.charAt(i) == 'a') || (str.charAt(i) == 'e') || (str.charAt(i) == 'i') ||
+                (str.charAt(i) == 'o') || (str.charAt(i) == 'u')) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
