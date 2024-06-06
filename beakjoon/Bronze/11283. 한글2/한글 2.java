3504bcb268675661807b7444e4c0b3b398d05977
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-06 12:58:38 +0900

    Create 한글 2.java

---
+++ beakjoon/Bronze/11283. 한글2/한글 2.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char c = br.readLine().charAt(0);
+
+        System.out.println((int)(c - 44031));
+    }
+}
