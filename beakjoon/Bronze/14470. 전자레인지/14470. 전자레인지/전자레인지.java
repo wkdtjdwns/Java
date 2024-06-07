5ea715b9e1476869be8f2b7dd60d1cdb261b8b25
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-07 18:48:05 +0900

    Create 전자레인지.java

---
+++ beakjoon/Bronze/14470. 전자레인지/14470. 전자레인지/전자레인지.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        int e = Integer.parseInt(br.readLine());
+        int sum = 0;
+        
+        if (a < 0) {
+            sum = (Math.abs(a) * c) + d + (b * e);
+        }
+        
+        else if (a == 0){
+            sum = d + (b * e);
+        }
+        
+        else {
+            sum = (b - a) * e;
+        }
+
+        System.out.println(sum);
+    }
+}
