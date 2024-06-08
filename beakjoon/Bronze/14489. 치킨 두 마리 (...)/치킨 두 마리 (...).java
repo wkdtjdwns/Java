fcb25ab942f748e3375852a97abbca3f157edb53
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-08 12:12:17 +0900

    Create 치킨 두 마리 (...).java

---
+++ beakjoon/Bronze/14489. 치킨 두 마리 (...)/치킨 두 마리 (...).java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(br.readLine());
+
+        System.out.println( (a + b) >= (c * 2) ? (a + b) - (c * 2) : a + b );
+    }
+}
