5439df0fe2cad422e8e5d0f77354fbe77d4fb18b
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-11 10:49:47 +0900

    Create 사장님 도박은 재미로 하셔야 합니다.java

---
+++ beakjoon/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        while (true) {
+            int money = Integer.parseInt(br.readLine());
+            if (money == -1) {
+                break;
+            }
+            
+            sum += money;
+        }
+
+        System.out.println(sum);
+    }
+}
