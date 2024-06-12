0ad0134597f0da21c9908732236aeabc55870413
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-12 17:14:27 +0900

    Create Dog.java

---
+++ school/after_school_class/Level 4/java.lang-pakage/object/toString/exam 2/Dog.java
@@ -0,0 +1,28 @@
+package lang.object.tostring;
+
+public class Dog {
+
+    private String dogName;
+    private int age;
+
+    public Dog(String dogName, int age) {
+        this.dogName = dogName;
+        this.age = age;
+    }
+
+    public String dogInfo() {
+        return "Dog{" +
+                "dogName='"+ dogName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public String toString() {
+        // 해당 클래스의 모든 정보를 알 수 있도록 toString() 메서드를 오버라이딩 함.
+        return "Dog{" +
+                "dogName='"+ dogName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
