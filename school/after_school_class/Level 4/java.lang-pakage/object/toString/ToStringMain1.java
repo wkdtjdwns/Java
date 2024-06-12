ae89bd36e6f697f0c0b3749426af0f7a17687a8e
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-12 16:44:53 +0900

    Create ToStringMain1.java

---
+++ school/after_school_class/Level 4/java.lang-pakage/object/toString/ToStringMain1.java
@@ -0,0 +1,21 @@
+package lang.object.tostring;
+
+public class ToStringMain1 {
+
+    public static void main(String[] args) {
+
+        Object object = new Object();
+        String string = object.toString();
+
+        // toString() 반환값 출력
+        System.out.println(string);
+
+        // object 직접 출력
+        System.out.println(object);
+
+        /*
+            java.lang.Object@1b6d3586
+            java.lang.Object@1b6d3586
+         */
+    }
+}
