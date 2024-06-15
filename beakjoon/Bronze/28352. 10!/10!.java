95e040b0f4b2dd5dc8990bd0b9694f0e15b38f8d
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-15 13:33:50 +0900

    [2024-06-15]

---
+++ beakjoon/Bronze/28352. 10!/10!.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(br.readLine());
+        long result = 1;
+
+        for (int i = 11; i < n + 1; i++) {
+            result *= i;
+        }
+
+        System.out.println(6 * result);
+    }
+}
