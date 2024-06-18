9b20ece2c6f1a4bd6c4e1ff611583db9193e54fa
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-18 10:41:19 +0900

    [2024-06-18]

---
+++ beakjoon/Bronze/30501. 관공... 어찌하여 목만 오셨소.../관공... 어찌하여 목만 오셨소....java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String name = br.readLine();
+
+            if (name.contains("S")) {
+                System.out.println(name);
+                break;
+            }
+        }
+    }
+}
