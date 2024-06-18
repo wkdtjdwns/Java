85f6ba54a0d7f750268ab8eff65aaa646344a70a
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-18 17:16:48 +0900

    [2024-06-18]

--- school/after_school_class/Level 4/java.lang-pakage/string/StringConcat/StringConcatMain.java
+++ school/after_school_class/Level 4/java.lang-pakage/string/StringConcat/StringConcatMain.java
@@ -8,11 +8,6 @@ public class StringConcatMain {
         String b = " java";
 
         String result1 = a.concat(b);
-
-        // +를 써도 자바가 알아서 concat()으로 바꿔줌.
-        // -> 결과가 같음.
-        // -> 실행 시간이 느려지기야 하겠지만 의미가 크지 않음.
-        // -> 대신 사용자가 월등하게 편해짐.
         String result2 = a + b;
 
         System.out.println("result1 = " + result1);
