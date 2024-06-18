003b2d1177e327c0e32731b19a00abb95acc9d5e
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-18 17:12:29 +0900

    [2024-06-18]

--- school/after_school_class/Level 4/java.lang-pakage/string/StringBasic/StringBasicMain.java
+++ school/after_school_class/Level 4/java.lang-pakage/string/StringBasic/StringBasicMain.java
@@ -11,10 +11,3 @@ public class StringBasicMain {
         System.out.println("str2 = " + str2);
     }
 }
-
-// 쌍따옴표 사용 방법, 객체 생성 방법이 있음.
-// String은 클래스임. int, boolean과 같은 기본형이 아니라 참조형임.
-// 따라서 str1 변수에는 String 인스턴스의 참조 값만 들어갈 수 있음.
-// 그러면 String str1 = "hello"; 문장은 뭔가 어색해 보임.
-// String str1 = "hello";
-// String str1 = new String("hello");
