60bc39310dc8c8e659c075980e889d8b3f72af5b
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-18 16:54:01 +0900

    [2024-06-18]

---
+++ school/after_school_class/Level 4/java.lang-pakage/string/CharArrayMain.java
@@ -0,0 +1,14 @@
+package lang.string;
+
+public class CharArrayMain {
+
+    public static void main(String[] args) {
+        // char를 이용하여 hello 출력
+        // String을 이용하여 hello 출력
+        char[] charHello = { 'h', 'e', 'l', 'l', 'o' };
+        String stringHello = "hello";
+
+        System.out.println(charHello);
+        System.out.println(stringHello);
+    }
+}
