63e871cb0f5ec179e3110eb1278b30a20acaae55
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-20 17:59:01 +0900

    [2024-06-20]

---
+++ school/after_school_class/Level 4/java.lang-pakage/string/exam/ex 1/teacherSolution.java
@@ -0,0 +1,26 @@
+package lang.string.Ex;
+
+import java.util.Scanner;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        char[] ans = str.toCharArray();
+
+        for (int i = 0; i < str.length(); i++) {
+            if ('a' <= ans[i] && ans[i] <= 'z') {
+                ans[i] = (char)('A' + ans[i] - 'a');
+            }
+
+            else {
+                ans[i] = (char)('a' + ans[i] - 'A');
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
