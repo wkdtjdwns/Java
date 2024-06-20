0cf5946ca3a14d062f1ce103289eebcabe1c732a
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-20 18:38:47 +0900

    [2024-06-20]

---
+++ school/after_school_class/Level 4/java.lang-pakage/string/exam/ex 5/teacherSolution.java
@@ -0,0 +1,34 @@
+package lang.string.Ex;
+
+import java.util.Scanner;
+
+public class TeacherEx5 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] current = sc.next().split(":");
+        String[] target = sc.next().split(":");
+
+        int currentHour = Integer.parseInt(current[0]);
+        int currentMinute = Integer.parseInt(current[1]);
+        int currentSecond = Integer.parseInt(current[2]);
+
+        int targetHour = Integer.parseInt(target[0]);
+        int targetMinute = Integer.parseInt(target[1]);
+        int targetSecond = Integer.parseInt(target[2]);
+
+        int currentSecondAmount = currentHour * 3600 + currentMinute * 60 + currentSecond;
+        int targetSecondAmount = targetHour * 3600 + targetMinute * 60 + targetSecond;
+
+        int needSecondAmount = targetSecondAmount - currentSecondAmount;
+        if (needSecondAmount <= 0) needSecondAmount += 24 * 3600;
+
+        int needHour = needSecondAmount / 3600;
+        int needMinute = (needSecondAmount & 3600) / 60;
+        int needSecond = needSecondAmount % 60;
+
+        System.out.printf("%02d:%02d:%02d", needHour, needMinute, needSecond);
+    }
+}
