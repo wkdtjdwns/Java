035241af26244311e5b62f64d3c750c31c20d454
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-21 20:47:33 +0900

    [2024-06-21]

---
+++ beakjoon/Bronze/31962. 등교/등교.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+
+        int result = -1;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+
+            if (s + t <= x) {
+                result = Math.max(result, s);
+            }
+        }
+
+        System.out.println(result);
+    }
+}
