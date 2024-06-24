3a92a4129d78bb2af08bb275d4bb1b0ee7d6228c
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-24 11:30:16 +0900

    [2024-06-24]

---
+++ beakjoon/Bronze/2442. 별 찍기 - 5/별 찍기 - 5.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < i * 2 - 1; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
