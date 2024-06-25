68a48e5fed5df593b70a7b3584a06b60aad708fd
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-25 20:02:06 +0900

    [2024-06-25]

---
+++ beakjoon/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        ArrayList<Integer> divisors = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                divisors.add(i);
+            }
+        }
+
+        int k = Integer.parseInt(st.nextToken());
+        if (divisors.size() >= k) {
+            System.out.println(divisors.get(k - 1));
+        }
+
+        else {
+            System.out.println(0);
+        }
+    }
+}
