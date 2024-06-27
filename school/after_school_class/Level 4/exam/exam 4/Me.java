d365f91177da74cd94ca3cc3908950570c36670c
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-27 17:29:29 +0900

    [2024-06-27]

---
+++ school/after_school_class/Level 4/exam/exam 4/Me.java
@@ -0,0 +1,42 @@
+package solveExam.exam4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Me {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashSet<String> listen = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            listen.add(br.readLine());
+        }
+
+        HashSet<String> see = new HashSet<>();
+        for (int i = 0; i < m; i++) {
+            see.add(br.readLine());
+        }
+
+        int cnt = 0;
+        ArrayList<String> ls = new ArrayList<>();
+        for (String name : listen) {
+            if (see.contains(name)) {
+                cnt++;
+                ls.add(name);
+            }
+        }
+
+        System.out.println(cnt);
+        Collections.sort(ls);
+        for (String name : ls) {
+            System.out.println(name);
+        }
+    }
+}
