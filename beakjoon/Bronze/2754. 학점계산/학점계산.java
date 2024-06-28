7ea6592d8b66e5ba9eef98437f101b72ee47120a
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-28 20:32:00 +0900

    [2024-06-28]

--- beakjoon/Bronze/2754. 학점계산/학점계산.java
+++ beakjoon/Bronze/2754. 학점계산/학점계산.java
@@ -1,40 +1,28 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String c = br.readLine();
-		switch (c) 
-		{
-		case "A+": System.out.println(4.3);
-		break;
-		case "A0": System.out.println(4.0);
-		break;
-		case "A-": System.out.println(3.7);
-		break;
-		case "B+": System.out.println(3.3);
-		break;
-		case "B0": System.out.println(3.0);
-		break;
-		case "B-": System.out.println(2.7);
-		break;	
-		case "C+": System.out.println(2.3);
-		break;
-		case "C0": System.out.println(2.0);
-		break;
-		case "C-": System.out.println(1.7);
-		break;
-		case "D+": System.out.println(1.3);
-		break;
-		case "D0": System.out.println(1.0);
-		break;
-		case "D-": System.out.println(0.7);
-		break;
-		case "F": System.out.println(0.0);
-		break;
-		}	
-	}
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String, Double> scroes = new HashMap<>();
+        scroes.put("A+", 4.3);
+        scroes.put("A0", 4.0);
+        scroes.put("A-", 3.7);
+        scroes.put("B+", 3.3);
+        scroes.put("B0", 3.0);
+        scroes.put("B-", 2.7);
+        scroes.put("C+", 2.3);
+        scroes.put("C0", 2.0);
+        scroes.put("C-", 1.7);
+        scroes.put("D+", 1.3);
+        scroes.put("D0", 1.0);
+        scroes.put("D-", 0.7);
+        scroes.put("F", 0.0);
+
+        System.out.println(scroes.get(br.readLine()));
+    }
 }
