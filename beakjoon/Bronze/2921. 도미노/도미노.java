7c1a9eda53f11a546ae241a32c6fc00571dc8613
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-30 17:01:59 +0900

    [2024-06-30]

---
+++ beakjoon/Bronze/2921. 도미노/도미노.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+                sum += i;
+                sum += j;
+            }
+        }
+        
+        System.out.println(sum);
+    }
+}
