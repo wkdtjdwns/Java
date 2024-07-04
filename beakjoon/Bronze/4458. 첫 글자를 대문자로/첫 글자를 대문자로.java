512223100048ae387526f8543d0080c995bb9682
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-04 18:59:42 +0900

    [2024-07-04]

---
+++ beakjoon/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            System.out.println((str.charAt(0) + "").toUpperCase() + str.substring(1, str.length()));
+        }
+    }
+}
