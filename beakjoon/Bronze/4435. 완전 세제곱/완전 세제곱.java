4fbc34571f358f2ed476b3d1ab7522ab181c76de
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-05 14:08:07 +0900

    [2024-07-05]

--- beakjoon/Bronze/4435. 완전 세제곱/완전 세제곱.java
+++ beakjoon/Bronze/4435. 완전 세제곱/완전 세제곱.java
@@ -1,17 +1,15 @@
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
         StringBuilder sb = new StringBuilder();
-
-        for(int a = 2; a <= 100; a++) {
-            for(int b = 2; b <= a; b++) {
-                for(int c = b; c <= a; c++) {
-                    for(int d = c; d <= a; d++) {
-                        if(a * a * a == b * b * b + c * c * c + d * d * d) {
+        for (int a = 2; a <= 100; a++) {
+            for (int b = 2; b <= a; b++) {
+                for (int c = b; c <= a; c++) {
+                    for (int d = c; d <= a; d++) {
+                        if (a * a * a == b * b * b + c * c * c + d * d * d) {
                             sb.append("Cube = ").append(a).append(", Triple = (").append(b);
                             sb.append(",").append(c).append(",").append(d).append(")").append("\n");
                         }
@@ -19,7 +17,7 @@ public class Main {
                 }
             }
         }
-        
+
         System.out.println(sb);
     }
 }
