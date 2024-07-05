b7620b10b62ea8d58ae02431bc7a89261806f942
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-05 14:19:36 +0900

    [2024-07-05]

---
+++ beakjoon/Bronze/4740. 거울, 오! 거울/거울, 오! 거울.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        while (true) {
+            String str = br.readLine();
+            if (str.equals("***")) {
+                break;
+            }
+            
+            for (int i = 0; i < str.length(); i++) {
+                sb.append(str.charAt((str.length() - 1) - i));
+            }
+            
+            sb.append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+}
