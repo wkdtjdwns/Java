602cc5530d03debe0772d84989e003f488c9084c
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-05 14:38:08 +0900

    [2024-07-05]

---
+++ beakjoon/Bronze/4880. 다음수/다음수.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0 && c == 0) {
+                break;
+            }
+
+            int n = b - a;
+            int m = c - b;
+
+            String result = (n == m) ? ("AP " + (c + m)) : ("GP " + (c + (m * (m / n))));
+            System.out.println(result);
+        }
+    }
+}
