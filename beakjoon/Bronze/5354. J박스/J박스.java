54e2fcb4d980237ede3fdecc507760ce001a729d
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-06 15:00:15 +0900

    [2024-07-06]

---
+++ beakjoon/Bronze/5354. J박스/J박스.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (j == 0 || j == n - 1 || k == 0 || k == n - 1) {
+                        System.out.print("#");
+                    }
+                    
+                    else {
+                        System.out.print("J");
+                    }
+                }
+                
+                System.out.println();
+            }
+            
+            System.out.println();
+        }
+    }
+}
