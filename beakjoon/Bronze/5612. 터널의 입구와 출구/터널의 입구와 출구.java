120135295079084e71086edabe007c5e9a7f84c2
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-07 15:57:06 +0900

    [2024-07-07]

---
+++ beakjoon/Bronze/5612. 터널의 입구와 출구/터널의 입구와 출구.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int max = m;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            m += a;
+            m -= b;
+            max = Math.max(max, m);
+            if (m < 0) {
+                max = 0;
+                break;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
