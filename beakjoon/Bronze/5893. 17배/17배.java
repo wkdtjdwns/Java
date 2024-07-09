f9c4f563015687e911b5e93ff24ab8dfc92c41b5
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-09 19:22:36 +0900

    [2024-07-09]

---
+++ beakjoon/Bronze/5893. 17배/17배.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger n = new BigInteger(br.readLine(), 2);
+        Stack<BigInteger> stack = new Stack<>();
+        
+        n = n.multiply(BigInteger.valueOf(17));
+
+        while (true) {
+            stack.push(n.remainder(BigInteger.valueOf(2)));
+            n = n.divide(BigInteger.valueOf(2));
+            if (n.equals(BigInteger.ONE)) {
+                stack.push(n);
+                break;
+            }
+        }
+        
+        while (!stack.isEmpty()) {
+            BigInteger s = stack.pop();
+            System.out.print(s);
+        }
+    }
+}
