c2d27f3526464168d3c7d765c7300e3125967d28
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-10 17:39:25 +0900

    [2024-07-10]

---
+++ school/after_school_class/Level 4/java.lang-pakage/math/MathMain.java
@@ -0,0 +1,17 @@
+package lang.math;
+
+public class MathMain {
+
+    public static void main(String[] args) {
+        System.out.println("Math.max(10, 20): " + Math.max(10, 20));
+        System.out.println("Math.min(10, 20): " + Math.min(10, 20));
+        System.out.println("Math.abs(-10): " + Math.abs(-10));
+
+        System.out.println("Math.ceil(2.1):" + Math.ceil(2.1));
+        System.out.println("Math.floor(2.7):" + Math.floor(2.7));
+        System.out.println("Math.round(2.5): " + Math.round(2.5));
+
+        System.out.println("Math.sqrt(4) = " + Math.sqrt(4));
+        System.out.println("Math.random() = " + Math.random());
+    }
+}
