996ed7b4240a75238fc74e8fdda38c78b747370f
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-10 17:41:01 +0900

    [2024-07-10]

---
+++ school/after_school_class/Level 4/java.lang-pakage/wrapper/exam/exam 1/WrapperTest1.java
@@ -0,0 +1,14 @@
+package lang.wrapper.test;
+
+public class WrapperTest1 {
+
+    public static void main(String[] args) {
+
+        String str1 = "10";
+        String str2 = "20";
+
+        int int1 = Integer.parseInt(str1);
+        int int2 = Integer.parseInt(str2);
+        System.out.println(int1 + int2);
+    }
+}
