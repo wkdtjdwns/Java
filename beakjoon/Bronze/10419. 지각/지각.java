2206c08101a09b62da2b4ad9158e99521d0360e9
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-14 16:49:05 +0900

    [2024-07-14]

---
+++ beakjoon/Bronze/10419. 지각/지각.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < t; i++) {
+            int d = Integer.parseInt(br.readLine());
+            int max = 1;
+            while(max + (max * max) <= d) {
+                max++;
+            }
+
+            System.out.println((max - 1));
+        }
+    }
+}
