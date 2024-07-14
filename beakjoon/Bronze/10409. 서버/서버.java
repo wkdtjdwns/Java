ff6777c8b0d50e044afcad73008f5b6eb35e04e9
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-14 16:33:49 +0900

    [2024-07-14]

---
+++ beakjoon/Bronze/10409. 서버/서버.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            int time = Integer.parseInt(st.nextToken());
+            if (t - time < 0) { break; }
+
+            t -= time;
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
