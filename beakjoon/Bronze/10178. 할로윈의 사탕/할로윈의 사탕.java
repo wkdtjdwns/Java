ae9d49506523f2682a29f4effca7ad3e8da5f320
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-14 16:25:03 +0900

    [2024-07-14]

---
+++ beakjoon/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int c = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            
+            sb.append(String.format("You get %d piece(s) and your dad gets %d piece(s).\n", c / v, c % v));
+        }
+        
+        System.out.print(sb);
+    }
+}
