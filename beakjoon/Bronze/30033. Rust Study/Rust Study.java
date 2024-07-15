a9c99e5c9f547934681dd53866bf1e8b4c784491
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-15 21:40:10 +0900

    [2024-07-15]

---
+++ beakjoon/Bronze/30033. Rust Study/Rust Study.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        int[] b = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] <= b[i]) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
