1060d7213d7c6a14d418883e18ebcb7e0f9967d9
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-15 21:55:04 +0900

    [2024-07-15]

--- beakjoon/Bronze/6359. 만취한 상범/만취한 상범.java
+++ beakjoon/Bronze/6359. 만취한 상범/만취한 상범.java
@@ -9,23 +9,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        for (int i = 1; i <=t; i++) {
+        for (int i = 1; i <= t; i++) {
             n = Integer.parseInt(br.readLine());
             d = new int[n + 1];
-            for (int j = 1; j<= n; j++) {
+            for (int j = 1; j <= n; j++) {
                 for (int k = 1; j * k <= n; k++) {
                     if (d[j * k] != 0) {
-                        d[j * k] =0;
+                        d[j * k] = 0;
                     }
 
                     else {
-                        d[j * k]=1;
+                        d[j * k] = 1;
                     }
                 }
             }
 
             int result = 0;
-            for(int j = 1; j<= n; j++) {
+            for(int j = 1; j <= n; j++) {
                 result += d[j];
             }
             
