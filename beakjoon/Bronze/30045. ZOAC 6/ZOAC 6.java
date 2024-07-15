c2e2378cc3c5d119663b90c8d01b18c14a740546
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-15 21:43:14 +0900

    [2024-07-15]

---
+++ beakjoon/Bronze/30045. ZOAC 6/ZOAC 6.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            if (str.contains("01") || str.contains("OI")) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
