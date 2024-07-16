4f6cde058b696db4a424eaac2215054c951a3446
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-16 18:06:09 +0900

    [2024-07-16]

---
+++ school/after_school_class/Level 4/java.lang-pakage/enumeration/exam 01/ClassGradeRefMain1.java
@@ -0,0 +1,14 @@
+package enumeration.ref1;
+
+public class ClassGradeRefMain1 {
+
+    public static void main(String[] args) {
+        
+        int price = 10_000;
+        
+        DiscountService discountService = new DiscountService();
+        int basic = discountService.discount(ClassGrade.BASIC, price);
+
+        System.out.println("basic = " + basic);
+    }
+}
