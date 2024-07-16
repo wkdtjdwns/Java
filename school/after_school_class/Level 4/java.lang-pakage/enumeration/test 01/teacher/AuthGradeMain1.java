9843c7b3669dd8a7459a4c816a9dc8e0a87133ca
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-16 18:40:28 +0900

    [2024-07-16]

---
+++ school/after_school_class/Level 4/java.lang-pakage/enumeration/test 01/teacher/AuthGradeMain1.java
@@ -0,0 +1,12 @@
+package enumeration.test.ex1;
+
+public class AuthGradeMain1 {
+
+    public static void main(String[] args) {
+
+        AuthGrade[] values = AuthGrade.values();
+        for (AuthGrade value : values) {
+            System.out.println("grade=" + value.name() + ", level=" + value.getLevel() + ", desc=" + value.getDesc());
+        }
+    }
+}
