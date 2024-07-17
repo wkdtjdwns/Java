5790c9fb6eadbda5f0bff52e286b98db027e5d94
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-17 10:07:33 +0900

    [2024-07-17]

--- school/after_school_class/Level 4/java.lang-pakage/enumeration/test 02/my solution/AuthGrade.java
+++ school/after_school_class/Level 4/java.lang-pakage/enumeration/test 02/my solution/AuthGrade.java
@@ -7,6 +7,7 @@ public enum AuthGrade {
 
     private final int level;
     private final String desc;
+    private final String[] menus = { "메인화면", "이메일 관리 화면", "관리자 화면" };
 
     AuthGrade(int level, String desc) {
         this.level = level;
@@ -19,5 +20,13 @@ public enum AuthGrade {
 
     public String getDesc() {
         return desc;
+    }
+
+    public void printInfo() {
+        System.out.println("==메뉴 목록==");
+        System.out.println("당신의 등급은 " + desc);
+        for (int i = 0; i < level; i++) {
+            System.out.println("- " + menus[i]);
+        }
     }
 }
