32fa33e6c14bae081424843b877ff27fd2f1a390
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-17 20:59:38 +0900

    [2024-07-17]

---
+++ beakjoon/Bronze/10865. 친구 친구/친구 친구.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] cnt = new int[n+1];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            cnt[Integer.parseInt(st.nextToken())]++;
+            cnt[Integer.parseInt(st.nextToken())]++;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(cnt[i]).append('\n');
+        }
+        
+        System.out.print(sb);
+    }
+}
