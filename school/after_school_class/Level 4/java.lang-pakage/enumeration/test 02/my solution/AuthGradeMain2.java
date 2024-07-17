10b3a5121474ff47238a1389e814cae082c68f09
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-17 10:07:44 +0900

    [2024-07-17]

--- school/after_school_class/Level 4/java.lang-pakage/enumeration/test 02/my solution/AuthGradeMain2.java
+++ school/after_school_class/Level 4/java.lang-pakage/enumeration/test 02/my solution/AuthGradeMain2.java
@@ -13,28 +13,6 @@ public class AuthGradeMain2 {
         AuthGrade[] values = AuthGrade.values();
         System.out.print("당신의 등급을 입력하세요" + Arrays.toString(values) + ": " );
         String grade = br.readLine().toUpperCase();
-        printInfo(AuthGrade.valueOf(grade));
-    }
-
-    private static void printInfo(AuthGrade authGrade) {
-        System.out.println("==메뉴 목록==");
-        System.out.println("당신의 등급은 " + authGrade.getDesc());
-        switch (authGrade) {
-            case GUEST:
-                System.out.println("- 메인 화면");
-                break;
-
-            case LOGIN:
-                System.out.println("- 메인 화면");
-                System.out.println("- 이메일 관리 화면");
-                break;
-
-
-            case ADMIN:
-                System.out.println("- 메인 화면");
-                System.out.println("- 이메일 관리 화면");
-                System.out.println("- 관리자 화면");
-                break;
-        }
+        AuthGrade.valueOf(grade).printInfo();
     }
 }
