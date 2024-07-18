ad0d7a6852a4be63f42e1bac50856dc70b54f01e
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-18 21:36:57 +0900

    [2024-07-18]

---
+++ beakjoon/Bronze/10953. A+B - 6/A+B - 6.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine(), ",");
+            System.out.println(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+        }
+    }
+}
