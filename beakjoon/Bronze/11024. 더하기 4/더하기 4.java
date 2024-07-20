f690919d1cdea21301ea1926ba4b8cb136b12b2c
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-20 18:37:18 +0900

    [2024-07-20]

---
+++ beakjoon/Bronze/11024. 더하기 4/더하기 4.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int total = 0;
+            for (int j = 0; j < 100; j++) {
+                if (!st.hasMoreElements()) {
+                    break;
+                }
+
+                total += Integer.parseInt(st.nextToken());
+            }
+
+            System.out.println(total);
+        }
+    }
+}
