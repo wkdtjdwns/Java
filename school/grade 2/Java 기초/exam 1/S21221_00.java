1d9165dec4b002fb2d9e006b1b03eb7cb3efe572
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-22 21:53:20 +0900

    [2024-07-22]

---
+++ school/grade 2/Java 기초/exam 1/S21221_00.java
@@ -0,0 +1,11 @@
+package S21221_00;
+
+public class S21221_00 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for (int cnt = 1; cnt < 11; cnt++) { sum += cnt; }
+		System.out.println("sum : " + sum);
+	}
+}
