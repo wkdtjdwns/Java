a1ae926292d48a557a48c947b2e701a6d6980446
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-22 14:20:19 +0900

    [2024-07-22]

---
+++ school/after_school_class/Level 4/Test/teacher solution/InternEmployee.java
@@ -0,0 +1,29 @@
+package level4.test.teacher;
+
+public class InternEmployee implements Employee {
+    private double baseSalary;
+    private EmployeeType employeeType;
+
+    public InternEmployee(double baseSalary) {
+        this.baseSalary = baseSalary;
+        this.employeeType = EmployeeType.INTERN;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return employeeType.adjustBaseSalary(baseSalary);
+    }
+
+    @Override
+    public EmployeeType getEmployeeType() {
+        return employeeType;
+    }
+
+    @Override
+    public String toString() {
+        return "InternEmployee{" +
+                "baseSalary=" + baseSalary +
+                ", employeeType=" + employeeType +
+                '}';
+    }
+}
