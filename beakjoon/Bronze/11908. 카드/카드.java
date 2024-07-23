d029fcffbd8986c90af501777f24d4f2679707f7
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-23 19:18:43 +0900

    [2024-07-23]

---
+++ beakjoon/Bronze/11908. 카드/카드.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+
+        int sum = 0;
+        for (int j = 0; j < n - 1; j++) {
+            sum += a[j];
+        }
+
+        System.out.println(sum);
+    }
+}
