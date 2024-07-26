9d9708541bbe605591714f7cc5cb3623d07dd5c0
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-26 21:39:15 +0900

    [2024-07-26]

---
+++ school/grade 2/조건문/조건문 기초/S21221_03.java
@@ -0,0 +1,31 @@
+package S21221_00;
+
+public class S21221_03 {
+
+	public static void main(String[] args) {
+		 int input = 0;
+	        
+		 if (input == 0) {
+			 System.out.println("a : 값은 0 입니다");
+		 }
+	        
+		 if (input != 0) {
+			 System.out.println("a : 값은 0이 아닙니다");
+		 }
+	        
+		 input = 3;
+	        
+		 if (input == 0) {
+			 System.out.println("b : 값은 0 입니다");
+		 } else {
+			 System.out.println("b : 값은 0이 아닙니다");
+		 }
+	}
+}
+
+/*
+실행 결과
+--------------------
+a : 값은 0 입니다
+b : 값은 0이 아닙니다
+*/
