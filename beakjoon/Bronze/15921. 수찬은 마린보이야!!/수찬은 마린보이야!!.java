41f42aea87628a6c9e17ab4c723cb63c4848613f
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-28 21:58:24 +0900

    [2024-07-28]

---
+++ beakjoon/Bronze/15921. 수찬은 마린보이야!!/수찬은 마린보이야!!.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if (n == 0) {
+            System.out.println("divide by zero");
+        }
+
+        else {
+            System.out.println("1.00");
+        }
+    }
+}
