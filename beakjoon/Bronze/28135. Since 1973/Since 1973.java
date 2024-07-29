3ceb7c7979985f4a3f4982285d93a0692e5e7b9a
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-29 18:01:14 +0900

    [2024-07-28]

---
+++ beakjoon/Bronze/28135. Since 1973/Since 1973.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result++;
+            if (Integer.toString(i).contains("50")) {
+                result++;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
