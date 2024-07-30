7cf39f95eb7a5c9de904a2522e9d2669e071c4d1
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-30 16:44:04 +0900

    [2024-07-30]

---
+++ beakjoon/Bronze/16504. 종이접기/종이접기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long result = 0;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                result += Integer.parseInt(st.nextToken());
+            }
+        }
+
+        System.out.println(result);
+    }
+}
