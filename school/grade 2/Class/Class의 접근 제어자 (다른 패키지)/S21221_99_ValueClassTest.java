71a2495118bb415234afe5ab6034e866f278ca47
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-01 21:29:58 +0900

    [2024-08-01]

---
+++ school/grade 2/Class/Class의 접근 제어자 (다른 패키지)/S21221_99_ValueClassTest.java
@@ -0,0 +1,43 @@
+package S21221_02;
+
+// 클래스 정의 (다른 클래스이기 때문에 따로 정의 해주어야 함)
+class Circle {
+	int radius;							// 자료형 필드 반지름
+	String color;						// 자료형 필드 색상
+	double calcArea() {					// 반환형 메소드
+		return 3.14 * radius * radius;	// 해당 값 리턴 
+	}
+}
+
+public class S21221_99_ValueClassTest {
+	
+	public static void main(String[] args){
+		
+		Circle a;					// 참조 변수 선언
+		a = new Circle();			// 객체 생성
+		a.radius = 100;				// 객체 필드 접근
+		a.color = "Blue";			// 객체 필드 접근
+		double area = a.calcArea(); // 객체 메소드 접근
+		System.out.println("a필드 색상 : " + a.color + " 원의 면적 = " + area);
+
+		Circle b;
+		b = new Circle();
+		b.radius = 20;
+		b.color = "White";
+		area = b.calcArea(); // 객체 메소드 접근
+		System.out.println("b필드 색상 : " + b.color + " 원의 면적 = " + area);
+
+		Circle c;
+		c = new Circle();
+		area = c.calcArea(); // 객체 메소드 접근
+		System.out.println("c필드 색상 : " + c.color + " 원의 면적 = " + area);
+	}
+}
+
+/*
+실행 결과
+------------------------------------
+a필드 색상 : Blue 원의 면적 = 31400.0
+b필드 색상 : White 원의 면적 = 1256.0
+c필드 색상 : null 원의 면적 = 0.0
+*/
