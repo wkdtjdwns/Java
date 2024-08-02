8da6ac7f9ec146f6480c27915270325939d706a1
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-02 18:16:58 +0900

    [2024-08-02]

---
+++ beakjoon/Bronze/20053. 최소, 최대 2/최소, 최대 2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                min = Math.min(min, num);
+                max = Math.max(max, num);
+            }
+
+            System.out.println(min + " " + max);
+        }
+    }
+}
