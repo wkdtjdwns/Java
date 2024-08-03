d138e4ed785681199ff0eea17c112f42bff0f22a
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-03 21:50:27 +0900

    [2024-08-03]

---
+++ beakjoon/Bronze/20361. 일우는 야바위꾼/일우는 야바위꾼.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == x) { x = b; }
+            else if (b == x) { x = a; }
+        }
+
+        System.out.println(x);
+    }
+}
