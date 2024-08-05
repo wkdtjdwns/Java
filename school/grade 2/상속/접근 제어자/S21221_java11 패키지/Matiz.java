e95a5322f406d058d6339a2920771854d1448feb
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-05 21:37:20 +0900

    [2024-08-05]

---
+++ school/grade 2/상속/접근 제어자/S21221_java11 패키지/Matiz.java
@@ -0,0 +1,10 @@
+package S21221_java11;
+
+import S21221_java10.Dog; // 패키지가 다르기 때문에 import 해서 상속함
+
+public class Matiz extends Dog {
+
+	public void a() {
+		System.out.println("5. Matiz_public 패키지를 넘나들며 멍과 바다에 갑니다.");
+	}
+}
