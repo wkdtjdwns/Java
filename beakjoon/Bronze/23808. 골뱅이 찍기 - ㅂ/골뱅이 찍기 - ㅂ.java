db28e5d5f998186551e9ff3fb381c5c190eae7df
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-05 16:22:01 +0900

    [2024-08-05]

---
+++ beakjoon/Bronze/23808. 골뱅이 찍기 - ㅂ/골뱅이 찍기 - ㅂ.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n * 2; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("@");
+            }
+
+            for (int j = 0; j < n * 3; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < n; j++) {
+                System.out.print("@");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n * 5; j++) {
+                System.out.print("@");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("@");
+            }
+
+            for (int j = 0; j < n * 3; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < n; j++) {
+                System.out.print("@");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n * 5; j++) {
+                System.out.print("@");
+            }
+
+            System.out.println();
+        }
+    }
+}
