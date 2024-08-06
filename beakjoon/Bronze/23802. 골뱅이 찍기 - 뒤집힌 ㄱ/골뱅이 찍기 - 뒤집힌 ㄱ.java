c0c5730ca70d67c3c0acab69792415926130bef7
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-06 16:54:12 +0900

    [2024-08-06]

---
+++ beakjoon/Bronze/23802. 골뱅이 찍기 - 뒤집힌 ㄱ/골뱅이 찍기 - 뒤집힌 ㄱ.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n * 5; j++) {
+                System.out.print("@");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < n * 4; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("@");
+            }
+
+            System.out.println();
+        }
+    }
+}
