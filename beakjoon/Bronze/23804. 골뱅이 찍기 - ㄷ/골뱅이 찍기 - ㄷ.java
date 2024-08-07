04aa049812d7da8e649c5e61592da89546b4e977
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-07 13:55:05 +0900

    [2024-08-07]

---
+++ beakjoon/Bronze/23804. 골뱅이 찍기 - ㄷ/골뱅이 찍기 - ㄷ.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n * 5; j++) {
+                System.out.print("@");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < n * 3; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("@");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n * 5; j++) {
+                System.out.print("@");
+            }
+
+            System.out.println();
+        }
+    }
+}
