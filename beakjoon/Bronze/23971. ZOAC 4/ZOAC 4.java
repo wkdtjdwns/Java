5151361f81345d402cd5e2261e59b130d3ce01d9
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-08 19:44:24 +0900

    [2024-08-08]

---
+++ beakjoon/Bronze/23971. ZOAC 4/ZOAC 4.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int f = (h -1) / (n +1) + 1;
+        int s = (w -1) / (m +1) + 1;
+        System.out.println(f * s);
+    }
+}
