e6456310b8120c0bbe1b7e7af91c15fa4cf2140a
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-20 20:30:32 +0900

    Create 모험의 시작.java

---
+++ beakjoon/Bronze/31789. 모험의 시작/모험의 시작.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        
+        boolean result = false;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int c = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+
+            if (x >= c && p > s) {
+                result = true;
+            }
+        }
+
+        System.out.println(result ? "YES" : "NO");
+    }
+}
