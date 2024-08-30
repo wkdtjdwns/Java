5d7823295f5f251fa2325921befcf9546aac80ad
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-30 23:28:22 +0900

    [2024-08-30]

---
+++ beakjoon/Bronze/23251. 스물셋/스물셋.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            long n = Long.parseLong(br.readLine());
+            System.out.println(n * 23);
+        }
+    }
+}
