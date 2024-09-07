fe63051e66d36bc7aec44e3d37c6629714195f75
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-09-07 17:55:38 +0900

    [2024-09-07]

---
+++ beakjoon/Bronze/23969. 알고리즘 수업 - 버블 정렬 2/알고리즘 수업 - 버블 정렬 2.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] a = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int cnt = 0;
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    cnt++;
+
+                    if (cnt == k) {
+                        for (int result : a) {
+                            System.out.print(result + " ");
+                        }
+                    }
+                }
+            }
+        }
+
+        if (cnt < k) {
+            System.out.println(-1);
+        }
+    }
+}
