b0b99c7fe5ef52f786379c698f42c397e607954d
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-09-27 12:23:57 +0900

    [2024-09-27]

---
+++ school/grade 2/예외 처리 (Exception handling)/try-catch문의 흐름/Ex8_1.java
@@ -0,0 +1,16 @@
+package ch08;
+
+public class Ex8_1 {
+
+	public static void main(String[] args) {
+		System.out.println(1 + " try블럭문 전");
+		try {
+			System.out.println(2 + " try블럭문");
+			System.out.println(3 + " try블럭문");
+		} catch (Exception e) {
+			System.out.println(4 + " catch블럭문 Exception e"); // 실행되지 않음.
+		}
+		
+		System.out.println(5 + " try블럭문 끝~~");
+	}
+}
